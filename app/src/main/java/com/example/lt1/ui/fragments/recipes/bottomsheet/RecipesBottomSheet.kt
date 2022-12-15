746027306c7631819d357cb6ee6b674fef75c8c5
Madhav Sharma <sharmamadhav208@gmail.com>
2022-12-15 23:56:52 +0530

    barebone commit of bottomsheet

---
+++ app/src/main/java/com/example/lt1/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -0,0 +1,21 @@
+package com.example.lt1.ui.fragments.recipes.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lt1.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class RecipesBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+    }
+
+
+}
