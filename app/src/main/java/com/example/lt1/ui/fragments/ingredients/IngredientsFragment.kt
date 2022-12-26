f6725857b9ef0ff4734bb0ea8baa1d6a8a23ae98
Madhav Sharma <sharmamadhav208@gmail.com>
2022-12-26 21:14:11 +0530

    "working instructions"

--- app/src/main/java/com/example/lt1/ui/fragments/ingredients/IngredientsFragment.kt
+++ app/src/main/java/com/example/lt1/ui/fragments/ingredients/IngredientsFragment.kt
@@ -5,18 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lt1.R
+import com.example.lt1.adapters.IngredientsAdapter
+import com.example.lt1.databinding.FragmentIngredientsBinding
+import com.example.lt1.models.Result
+import com.example.lt1.util.Constants.Companion.RECIPE_RESULT_KEY
 
 
 class IngredientsFragment : Fragment() {
 
+    private val mAdapter: IngredientsAdapter by lazy { IngredientsAdapter() }
+
+    private var _binding: FragmentIngredientsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ingredients, container, false)
+        _binding = FragmentIngredientsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(RECIPE_RESULT_KEY)
+
+        setupRecyclerView()
+        myBundle?.extendedIngredients?.let { mAdapter.setData(it) }
+
+        return binding.root
     }
 
+    private fun setupRecyclerView() {
+        binding.ingredientsRecyclerview.adapter = mAdapter
+        binding.ingredientsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
