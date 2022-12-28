c690ec98f8a941d6c75a8763f098781a74e30008
Madhav Sharma <sharmamadhav208@gmail.com>
2022-12-28 23:53:00 +0530

    "working instructions"

--- app/src/main/java/com/example/lt1/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/lt1/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,18 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.example.lt1.R
+import com.example.lt1.databinding.FragmentIngredientsBinding
+import com.example.lt1.databinding.FragmentInstructionsBinding
+import com.example.lt1.models.Result
+import com.example.lt1.util.Constants
 
 
 class InstructionsFragment : Fragment() {
-
+    private var _binding:FragmentInstructionsBinding?=null
+    private val binding get()=_binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        _binding= FragmentInstructionsBinding.inflate(inflater,container,false)
+        val view=binding.root
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+        binding.instructionsWebView.webViewClient=object :WebViewClient(){}
+        val websiteUrl:String=myBundle!!.sourceUrl
+        binding.instructionsWebView.loadUrl(websiteUrl)
+
+        return view
     }
 
 
